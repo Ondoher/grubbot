ccf7db642b442ba0edc1dbbdba3f4dfba0c50c03
Glenn <ondoher@gmail.com>
2018-11-15 09:34:03 -0800

    Added some documentation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # grubbot
-The Grub Lunch Survey Bot
+The Lunch Survey Bot
+
+This application was generated and is maintained using [symphony-app](https://github.com/Ondoher/symphony-app)
--- applications/apps/grubbot/features/services/assets/js/Services/Renderer.js
+++ applications/apps/grubbot/features/services/assets/js/Services/Renderer.js
@@ -2,6 +2,7 @@ Package('Grubbot.Services', {
 	Renderer : new Class({
 		implements : ['render', 'action', 'reaped'],
 
+	// class constructor
 		initialize : function()
 		{
 			this.serviceName = 'grubbot:renderer';
@@ -16,16 +17,19 @@ Package('Grubbot.Services', {
 			GRUBBOT.events.listen('start', this.onReady.bind(this));
 		},
 
+	// called to track an entity that can be updated
 		track : function(entity)
 		{
 			this.tracked[entity.instanceId] = entity;
 		},
 
+	// called to untrack an entity that will no longer be updated
 		untrack : function(instanceId)
 		{
 			delete this.tracked[instanceId];
 		},
 
+	// on the timer, update all tracked entities
 		tick : function()
 		{
 			Object.each(this.tracked, function(entity)
@@ -57,6 +61,7 @@ Package('Grubbot.Services', {
 				.then(this.getResults.bind(this, entityData))
 		},
 
+	// called in response to a star being clicked
 		action: function(data)
 		{
 			this.voteModel.vote(data.entity.id, GRUBBOT.userId, data.vote)
@@ -70,6 +75,11 @@ Package('Grubbot.Services', {
 
 		renderVoting : function(entityData, instanceId)
 		{
+		// template is the extentionML template, data is values for the template.
+		// The keys of this object correspond to the id attribute on template tags
+
+		// the stars on the voting state are implemented as actions. The data field is
+		// passed to the action method of the specified service
 			var result = {
 				template: this.votingXml,
 				data: {
@@ -166,10 +176,14 @@ Package('Grubbot.Services', {
 			return stars;
 		},
 
+	// render the entity state after voting has closed
 		renderClosed : function(entityData, voteData)
 		{
 			var average = voteData.result.average;
 			var stars = this.getStars(average);
+
+		// template is the extentionML template, data is values for the template.
+		// The keys of this object correspond to the id attribute on template tags
 			var result = {
 				template: this.closedXml,
 				data: {
@@ -186,10 +200,14 @@ Package('Grubbot.Services', {
 			return result;
 		},
 
+	// render the entity state after the user has voted but before voting has closed
 		renderVoted : function(entityData, voteData)
 		{
 			var average = voteData.result.average;
 			var stars = this.getStars(average);
+
+		// template is the extentionML template, data is values for the template.
+		// The keys of this object correspond to the id attribute on template tags
 			var result = {
 
 				template: this.votedXml,
@@ -205,20 +223,28 @@ Package('Grubbot.Services', {
 			return result;
 		},
 
+	// the entity renderer for Grubbot.
 		render : function(type, entityData)
 		{
+		// create a unique instance string so we can update the rendered entity
 			var instanceId = String.uniqueID();
+
+		// get the latest voting data
 			return this.getVoteData(entityData)
 				.then(function(response)
 				{
 					var result;
 
+				// render the entity state
 					if (entityData.end < Date.now()) result = this.renderClosed(entityData, response);
 					else if (!response.vote) result = this.renderVoting(entityData, instanceId);
 					else result = this.renderVoted(entityData, response);
 
+				// return the instance ID as part of the result so that Symphony can track
+				// the rendered entity for later updating
 					result.entityInstanceId = instanceId;
 
+				// If the voting has not closed, track the new entity
 					if (entityData.end < Date.now() &&  response)
 						setTimeout(function()
 						{
@@ -229,7 +255,7 @@ Package('Grubbot.Services', {
 				}.bind(this));
 		},
 
-
+	// Called by the update timer to rerender an entity with the latest voting data
 		rerender : function(tracked)
 		{
 			var entityData = tracked.entityData;
@@ -245,35 +271,48 @@ Package('Grubbot.Services', {
 
 					if (entityData.end < Date.now()) this.untrack(tracked.instanceId);
 
+				// update the entity with new rendering data. Uses the instance id created when the
+				// entity was first rendered
 					return this.entity.update(tracked.instanceId, result.template, result.data);
 				}.bind(this));
 		},
 
+	// after voting has closed, stop tracking the entity for updating
 		reaped : function(what, type, id, messageId) {
 			this.untrack(id);
 		},
 
+	// This method is called by the Sapphire framework when the DOM is fully loaded
+	// but before the application is registered with the Symphony client
 		onStart : function(done)
 		{
 			var bootstrap = SYMPHONY.services.subscribe('bootstrap');
 
+		// specify the required Symphony services
 			this.importServices.each(function(service)
 			{
 				bootstrap.importService(service);
 			}, this);
 
+		// export our service so it can be imported by the Symphony client
 			bootstrap.exportService(this.serviceName);
 			done();
 		},
 
+	// This method is called after the application has been registered with the
+	// Symphony client. All required services have been imported and are available.
 		onReady : function()
 		{
+		// get or templates
 			this.votingXml = GRUBBOT.votingTemplate;
 			this.votedXml = GRUBBOT.votedTemplate;
 			this.closedXml = GRUBBOT.closedTemplate;
 
+		// register our entity renderer
 			this.entity = SYMPHONY.services.subscribe('entity');
 			this.entity.registerRenderer('com.symphony.grubbot.feedback', {}, this.serviceName);
+
+		// set up timer to update rendered entities with voting results
 			this.interval = setInterval(this.tick.bind(this), 5 * 60 * 1000);
 		}
 	})
